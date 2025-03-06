98a64af9574c47a14ed12eb24b9c4e19d2d8f3ab
Paoak <horusadeptus@gmail.com>
2025-03-07 04:55:50 +0600

    tg_bot v0.0.1 config update

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
-# config.py
-API_TOKEN = '7911033004:AAE2WwnQquPChMTYqKuwFQxEaNs0yzS6gMw'  # Замените на свой токен
+API_TOKEN = 'Замените на свой токен'
 DB_NAME = 'quiz_bot.db'
-BOT_USERNAME = '@test_quiz_game_bot'  # Замените на имя вашего бота
+BOT_USERNAME = '@Замените на имя вашего бота'
+
+
+
+# https://github.com/Paoak/TelegramQuizBot.git
